5c3cfff61039cf933efb056b1d48fbf21f39d0d8
Joan Gavelán <joangavelan@gmail.com>
2021-08-03 18:56:17 -0500

    header section done

---
+++ src/components/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import logo from '../assets/images/logo.png'
+import './Logo.scss'
+
+const Logo = () => <img className="Logo" src={logo} alt="logo"/>;
+
+export default Logo
--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Header.scss';
 import Nav from '../components/Nav'
+import Logo from '../components/Logo'
 
 const Header = () => {
   return (
     <header className="header">
       <Nav />
+      <Logo />
     </header>
   )
 }
