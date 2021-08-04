6d36361bc8ce859be81f05645c5db9d4bb600ada
Joan Gavelán <joangavelan@gmail.com>
2021-08-03 19:11:42 -0500

    header section done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./sections/Header";
+import Introduction from "./sections/Introduction";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Introduction />
     </div>
   );
 }
--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import './Header.scss';
 import Nav from '../components/Nav'
 import Logo from '../components/Logo'
+import backgroundURL from '../assets/images/desktop/header-bg-desktop.jpg';
+
+const styles = {
+  background: `url(${backgroundURL}) no-repeat center`,
+  backgroundSize: 'cover',
+  height: '240rem'
+}
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header" style={styles}>
       <Nav />
       <Logo />
     </header>
